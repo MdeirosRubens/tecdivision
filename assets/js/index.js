d5c61a069fb4df5b980de38851592cb086de949d
MdeirosRubens <mdeirosrubens@outlook.com>
2022-01-09 20:06:25 -0300

    organizando estrutura e concluindo a home

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
-const botaoMenu = document.querySelector('.menu__abrir')
-const menu = document.querySelector('.menu-lateral')
+const botaoMenu = document.querySelector('.menu__abrir');
+const menu = document.querySelector('.menu-lateral');
+const imgBody = document.querySelector('.logo_body');
 
 botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-lateral--ativo')
+    menu.classList.toggle('menu-lateral--ativo');
 })
